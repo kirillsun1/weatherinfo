a9b52a295d15892428e1fca899f8421eddc9a115
Kirill <kideni@ttu.ee>
2017-09-20 19:19:26 +0300

    connection test

---
+++ src/main/java/network/HTTPConnection.java
@@ -0,0 +1,9 @@
+package network;
+
+import java.net.HttpURLConnection;
+
+public class HTTPConnection {
+    public static HttpURLConnection createConnection(String url) {
+        return null;
+    }
+}
---
+++ src/test/java/network/HTTPConnectionTest.java
@@ -0,0 +1,39 @@
+package network;
+
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.HttpURLConnection;
+
+import static org.junit.Assert.*;
+
+public class HTTPConnectionTest {
+    @Test
+    public void testConnectionToAPI() {
+        HttpURLConnection con = HTTPConnection.createConnection("http://api.openweathermap.org/data/2.5");
+        try {
+            int responseCode = con.getResponseCode();
+
+            assertEquals(HttpURLConnection.HTTP_OK, responseCode);
+        } catch (IOException e) {
+            fail("Exception raised: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testConnectionToNotExistingLink() {
+        HttpURLConnection con = HTTPConnection.createConnection("nolink.es");
+        try {
+            int responseCode = con.getResponseCode();
+            assertEquals(HttpURLConnection.HTTP_NOT_FOUND, responseCode);
+        } catch (IOException e) {
+            fail("Exception raised: " + e.getMessage());
+        }
+    }
+
+    @Test
+    public void testConnectionToNullLink() {
+        HttpURLConnection con = HTTPConnection.createConnection("nolink.es");
+        assertNull(con);
+    }
+}
